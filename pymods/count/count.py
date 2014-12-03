8399d2028b178a71c4d958915b8208db8706238b
alectramell <alectramell@gmail.com>
2014-12-03 03:24:23 -0700

    DARKTIE

--- pymods/count/count.py
+++ pymods/count/count.py
@@ -2,12 +2,10 @@
 
 import os
 
-def count():
+def word():
     os.system('clear')
     ask = raw_input('(word): ')
     x = len(ask)
     os.system('clear')
     x = str(x)
     print ('"' + ask + '" is ' + x + ' character(s) long!')
-
-count()
